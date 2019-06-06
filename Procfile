09c70fe31e7de18ad1bb37f2206b6e1349cb89f9
СапоГ <katenochekspb@mail.ru>
2019-06-06 12:58:27 +0300

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/telebot-1.0-SNAPSHOT.jar
---
+++ src/main/java/ru/ariona/Bot.java
@@ -0,0 +1,50 @@
+package ru.ariona;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public String getBotUsername() {
+        return "ArionaBot";
+    }
+
+    public String getBotToken() {
+        return "823926417:AAEdy_gHi38kLHVaa_juw2s8Oq6r46BbZzg";
+    }
+
+
+}
